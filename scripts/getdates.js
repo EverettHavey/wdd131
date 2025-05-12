db8ece1a55adcfbf9646e9929b17515790beae8a
Everett <ehavey@byupathway.edu>
2025-05-12 12:50:58 -0700

    Fixed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -16,6 +16,24 @@ full.innerHTML = `Last Modified <span class="highlight">${new Intl.DateTimeForma
 		dateStyle: "full"
 	}
 ).format(today)}</span>`;
+worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-UK",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
+medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "medium"
+	}
+).format(today)}</span>`;
 
 year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
 month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
