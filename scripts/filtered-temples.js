0a57f3cec16ff44f57e4cfd8f5040da1e8698497
Everett <ehavey@byupathway.edu>
2025-06-03 01:54:31 -0700

    Fixed

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,60 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const yearSpan = document.getElementById('year');
-  if (yearSpan) {
-    const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear;
-  }
-
-  const modifiedSpan = document.querySelector('#full .highlight');
-  if (modifiedSpan) {
-    const lastModified = new Date(document.lastModified);
-    modifiedSpan.textContent = lastModified.toLocaleDateString() + ' ' + lastModified.toLocaleTimeString();
-  }
-
-  const hamburgerButton = document.createElement('button');
-  hamburgerButton.classList.add('hamburger-button');
-  hamburgerButton.innerHTML = '&#9776;';
-
-  const closeButton = document.createElement('button');
-  closeButton.classList.add('close-button');
-  closeButton.innerHTML = '&#10006;';
-  closeButton.style.display = 'none';
-
-  const nav = document.querySelector('nav');
-  const navUl = document.querySelector('nav ul');
-
-  if (nav) {
-    nav.insertBefore(hamburgerButton, navUl);
-    nav.insertBefore(closeButton, navUl);
-
-    hamburgerButton.addEventListener('click', () => {
-      navUl.classList.toggle('open');
-      hamburgerButton.style.display = 'none';
-      closeButton.style.display = 'block';
-    });
-
-    closeButton.addEventListener('click', () => {
-      navUl.classList.remove('open');
-      closeButton.style.display = 'none';
-      hamburgerButton.style.display = 'block';
-    });
-
-    window.addEventListener('resize', () => {
-      if (window.innerWidth >= 768) {
-        navUl.classList.remove('open');
-        closeButton.style.display = 'none';
-        hamburgerButton.style.display = 'none'; 
-      } else if (!navUl.classList.contains('open')) {
-        hamburgerButton.style.display = 'block'; 
-      }
-    });
-
-    if (window.innerWidth >= 768) {
-      hamburgerButton.style.display = 'none';
-    }
-  }
-});
-
 const temples = [
   {
     templeName: "Aba Nigeria",
@@ -116,12 +59,12 @@ const temples = [
   // More Temples...//
 
   {
-    templeName: "Las Vegas Nevada Temple",
-    location: "Las Vegas, Nevada",
-    dedicated: "16–18 December 1989",
-    area: 10.3,
-    imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  templeName: "Las Vegas Nevada Temple",
+  location: "Las Vegas, Nevada",
+  dedicated: "16–18 December 1989",
+  area: 10.3,
+  imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/las-vegas-nevada/400x250/las-vegas-temple-exterior-001-wallpaper.jpg"
   },
   {
     templeName: "Mexico City Mexico",
